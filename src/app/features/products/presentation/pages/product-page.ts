0b1bede9ad897da9d5f8090f4f2e82c9e122a147
RKOrtega <rkortega1994@gmail.com>
2025-06-02 19:00:01 -0500

    feat: implement product page layout with header and main sections, add button component with tests

--- src/app/features/products/presentation/pages/product-page.ts
+++ src/app/features/products/presentation/pages/product-page.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ProductFilter } from "../components/product-filter/product-filter";
+import { ProductList } from "../components/product-list/product-list";
 
 @Component({
   selector: 'app-product-page',
-  imports: [],
+  imports: [ProductFilter, ProductList],
   templateUrl: './product-page.html',
   styleUrl: './product-page.css'
 })
