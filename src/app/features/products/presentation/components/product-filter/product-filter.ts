96708cc879211748cbc8b6978a91f16d42156228
RKOrtega <rkortega1994@gmail.com>
2025-06-02 20:10:19 -0500

    feat: implement product filter component with text field and styles

--- src/app/features/products/presentation/components/product-filter/product-filter.ts
+++ src/app/features/products/presentation/components/product-filter/product-filter.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { TextField } from '@app/shared/components/fields/text-field/text-field';
 
 @Component({
   selector: 'app-product-filter',
-  imports: [],
+  imports: [TextField],
   templateUrl: './product-filter.html',
-  styleUrl: './product-filter.css'
+  styleUrl: './product-filter.css',
 })
-export class ProductFilter {
-
-}
+export class ProductFilter {}
