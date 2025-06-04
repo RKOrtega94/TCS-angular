2aa6e65dc4363f7b456feb5a086b081b76d0c46b
RKOrtega <rkortega1994@gmail.com>
2025-06-03 21:10:34 -0500

    feat: enhance API service methods to handle ApiResponse and errors

--- src/app/core/api/api.service.ts
+++ src/app/core/api/api.service.ts
@@ -1,13 +1,17 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, map, Observable, throwError } from 'rxjs';
+import { ApiResponse } from './api.response.interface';
 
 @Injectable({ providedIn: 'root' })
 export abstract class ApiService {
   #http = inject(HttpClient);
 
   GET<T>(url: string, params?: Record<string, any>): Observable<T> {
-    return this.#http.get<T>(url, { params });
+    return this.#http.get<ApiResponse<T>>(url, { params }).pipe(
+      map((response) => response.data),
+      catchError(this.#hadleError)
+    );
   }
 
   POST<T>(
@@ -15,14 +19,28 @@ export abstract class ApiService {
     body?: any,
     params?: Record<string, any>
   ): Observable<T> {
-    return this.#http.post<T>(url, body, { params });
+    return this.#http.post<ApiResponse<T>>(url, body, { params }).pipe(
+      map((response) => response.data),
+      catchError(this.#hadleError)
+    );
   }
 
   PUT<T>(url: string, body?: any, params?: Record<string, any>): Observable<T> {
-    return this.#http.put<T>(url, body, { params });
+    return this.#http.put<ApiResponse<T>>(url, body, { params }).pipe(
+      map((response) => response.data),
+      catchError(this.#hadleError)
+    );
   }
 
   DELETE<T>(url: string, params?: Record<string, any>): Observable<T> {
-    return this.#http.delete<T>(url, { params });
+    return this.#http.delete<ApiResponse<T>>(url, { params }).pipe(
+      map((response) => response.data),
+      catchError(this.#hadleError)
+    );
+  }
+
+  #hadleError(error: HttpErrorResponse): Observable<never> {
+    console.error(error);
+    return throwError(() => new Error(error.message));
   }
 }
