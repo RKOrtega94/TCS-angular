58341673a5041ca3ebf646da390f16386b718d27
RKOrtega <rkortega1994@gmail.com>
2025-06-03 20:22:15 -0500

    feat: add proxy configuration and implement API response interface

--- src/app/features/products/data/repositories/product.repository.impl.ts
+++ src/app/features/products/data/repositories/product.repository.impl.ts
@@ -2,18 +2,18 @@ import { Injectable } from '@angular/core';
 import { IProductRepository } from '../../domain/product.repository';
 import { Observable, of } from 'rxjs';
 import { Product } from '../../domain/product.model';
+import { ApiService } from '@app/core/api/api.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ProductRepositoryImpl implements IProductRepository {
-  private products: Product[] = [
-    { id: '1', name: 'Product 1', description: 'Description 1' },
-    { id: '2', name: 'Product 2', description: 'Description 2' },
-    { id: '3', name: 'Product 3', description: 'Description 3' },
-  ];
+export class ProductRepositoryImpl
+  extends ApiService
+  implements IProductRepository
+{
+  #basePath: string = 'bp/products';
 
   getAll(): Observable<Product[]> {
-    return of(this.products);
+    return this.GET<Product[]>(this.#basePath);
   }
 }
