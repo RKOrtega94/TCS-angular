43e45839d1405afd5f36f0b872aa99ed2acb87d3
RKOrtega <rkortega1994@gmail.com>
2025-06-03 22:07:52 -0500

    feat: update proxy configuration and enhance API interceptor for dynamic URL handling

--- src/app/core/api/api-interceptor.ts
+++ src/app/core/api/api-interceptor.ts
@@ -2,9 +2,11 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { environment } from 'environments/environment';
 
 export const apiInterceptor: HttpInterceptorFn = (req, next) => {
-  const apiUrl = `${environment.apiUrl}/${req.url}`;
+  const isProd = environment.apiUrl !== '' && window.location.port !== '4200';
+  const apiUrl = isProd ? `${environment.apiUrl}/${req.url}` : req.url;
   const apiReq = req.clone({
     url: apiUrl,
+    headers: req.headers,
   });
   return next(apiReq);
 };
